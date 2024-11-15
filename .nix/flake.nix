9c6c8984d52707a40f1009f7eed95dd988a9ff93
Mark Mekosh <markmekosh@gmail.com>
2024-11-15 16:56:05 -0500

    Initial commit

---
+++ .nix/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        jdk
+        python3
+      ];
+
+      shellHook = ''
+        export PATH="$PATH:$HOME/code/dsa/algorithms/external/mybin:$HOME/code/dsa/algorithms/external/lift"
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/code/dsa/algorithms/external/mybin:$HOME/code/dsa/algorithms/external/lift"
+      '';
+    };
+
+  };
+}
