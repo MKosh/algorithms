5e88ac6026cb9bfb31ef6b11208df9cbb87b5d49
Mark Mekosh <markmekosh@gmail.com>
2025-04-26 12:01:22 -0400

    array stack in zig

--- src/ch1/mod3/stack.zig
+++ src/ch1/mod3/stack.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
 const expect = std.testing.expect;
+const Allocator = std.mem.Allocator;
 
 pub fn fixedStack(comptime T: type, capacity: i32) type {
     return struct {
@@ -27,52 +28,65 @@ pub fn fixedStack(comptime T: type, capacity: i32) type {
     };
 }
 
-pub fn ResizingArrayStack(comptime T: type) type {
+// Based on the stack implementation from the zig book
+pub fn Stack(comptime T: type) type {
     return struct {
+        items: []T,
+        capacity: usize,
+        length: usize,
+        allocator: Allocator,
         const Self = @This();
-        data: [1]T,
-        size: u32,
 
-        pub fn init() Self {
-            return Self{ .data = [1]T{0}, .size = 0 };
+        pub fn init(allocator: Allocator, capacity: usize) !Stack(T) {
+            var buffer = try allocator.alloc(T, capacity);
+            return .{ .items = buffer[0..], .capacity = capacity, .length = 0, .allocator = allocator };
         }
 
-        pub fn isEmpty(self: Self) bool {
-            return self.size == 0;
-        }
-
-        pub fn getSize(self: Self) u32 {
-            return self.size;
-        }
-
-        pub fn push(self: *Self, value: T) void {
-            if (self.size == self.data.len) {
-                self.resize(2 * self.data.len);
+        pub fn push(self: *Self, value: T) !void {
+            if (self.length + 1 > self.capacity) {
+                var temp = try self.allocator.alloc(T, self.capacity * 2);
+                @memcpy(temp[0..self.capacity], self.items);
+                self.allocator.free(self.items);
+                self.items = temp;
+                self.capacity = self.capacity * 2;
             }
-            self.data[self.size] = value;
-            self.size += 1;
+
+            self.items[self.length] = value;
+            self.length += 1;
         }
 
-        fn resize(self: *Self, max: u32) void {
-            // var temp = [_]T{0} ** max;
-            // var temp = std.mem.zeroes([max]T);
-            var temp = [1]T{0} ** max;
-
-            for (self.data, 0..) |elem, i| {
-                temp[i] = elem;
+        pub fn pop(self: *Self) ?u32 {
+            if (self.length == 0) {
+                return null;
             }
-            self.data = temp;
+            const temp = self.items[self.length - 1];
+            self.items[self.length - 1] = undefined;
+            self.length -= 1;
+            return temp;
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.allocator.free(self.items);
         }
     };
 }
 
 pub fn main() !void {}
 
-test "Resizing stack" {
-    var stack = ResizingArrayStack(i32).init();
-    stack.push(1);
-    print("index: {d}, value: {d}\n", .{ 0, stack.data[0] });
-    try expect(true);
+test "Stack" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var stack = try Stack(u32).init(allocator, 10);
+    try stack.push(1);
+    try stack.push(3);
+    try expect(stack.capacity == 10);
+    try expect(stack.items[0] == 1);
+    try expect(stack.items[1] == 3);
+    try expect(stack.pop() == 3);
+    try expect(stack.pop() == 1);
+    try expect(stack.pop() == null);
+    try expect(stack.length == 0);
+    stack.deinit();
 }
 
 test "fixed stack" {
